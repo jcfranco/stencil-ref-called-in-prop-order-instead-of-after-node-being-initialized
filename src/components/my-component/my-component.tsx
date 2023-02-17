b85c2f478c6f52994e7414a216e080bdceced868
JC Franco <jfranco@esri.com>
2023-02-17 10:04:16 -0800

    set up repro case

--- src/components/my-component/my-component.tsx
+++ src/components/my-component/my-component.tsx
@@ -1,32 +1,17 @@
-import { Component, Prop, h } from '@stencil/core';
-import { format } from '../../utils/utils';
+import { Component, h } from '@stencil/core';
 
 @Component({
   tag: 'my-component',
-  styleUrl: 'my-component.css',
   shadow: true,
 })
 export class MyComponent {
-  /**
-   * The first name
-   */
-  @Prop() first: string;
-
-  /**
-   * The middle name
-   */
-  @Prop() middle: string;
-
-  /**
-   * The last name
-   */
-  @Prop() last: string;
-
-  private getText(): string {
-    return format(this.first, this.middle, this.last);
+  storeRef(el) {
+    console.log(el.outerHTML, el.tabIndex);
   }
-
   render() {
-    return <div>Hello, World! I'm {this.getText()}</div>;
+    return <div ref={this.storeRef} tabIndex={0}>simple div</div>
+
+    // workaround: make sure ref is specified last
+    // return <div tabIndex={0} ref={this.storeRef}}>simple div</div>
   }
 }
